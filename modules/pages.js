1652edd84cfaef7651e2601509731734372e5d29
Elfin Git <musongoletracy@gmail.com>
2022-08-07 16:23:29 +0300

    add pages.js

---
+++ modules/pages.js
@@ -0,0 +1,32 @@
+// Hide and display different pages //
+const homepage = document.querySelector('.home');
+const addNew = document.querySelector('.add');
+const contactpage = document.querySelector('.contact');
+const homeSection = document.querySelector('#book-list');
+const bookSection = document.querySelector('#Add-book');
+const contactSection = document.querySelector('#contactUs');
+const Clicked = false;
+
+homepage.addEventListener('click', () => {
+  if (Clicked === false) {
+    homeSection.style.display = 'block';
+    bookSection.style.display = 'none';
+    contactSection.style.display = 'none';
+  }
+});
+
+addNew.addEventListener('click', () => {
+  if (Clicked === false) {
+    homeSection.style.display = 'none';
+    bookSection.style.display = 'block';
+    contactSection.style.display = 'none';
+  }
+});
+
+contactpage.addEventListener('click', () => {
+  if (Clicked === false) {
+    homeSection.style.display = 'none';
+    bookSection.style.display = 'none';
+    contactSection.style.display = 'block';
+  }
+});
