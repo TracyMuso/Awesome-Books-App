46408d3db8a1e14452c3131e0dcab4a6330435bd
Elfin Git <musongoletracy@gmail.com>
2022-08-07 16:46:16 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Show date and time
+window.onload = () => {
+  const date = new Date();
+  date.setDate(date.getDate());
+  document.querySelector('.date').innerHTML = date;
+  
+  function updateTime() {
+    const time = document.getElementsByClassName('date');
+    const datestring = new Date().toLocaleString();
+    const newString = datestring.replace(', ', ' -');
+    time.textContent = newString;
+  }
+  setInterval(updateTime, 1000);
+};
---
+++ modules/library.js
@@ -0,0 +1,48 @@
+class Library {
+  constructor() {
+    this.collection = JSON.parse(localStorage.getItem('books')) || [];
+    this.checkLength();
+  }
+  
+  checkLength() {
+    const empty = document.querySelector('.empty');
+    if (this.collection.length === 0) {
+      empty.innerHTML = 'The library is empty';
+    } else {
+      empty.innerHTML = '';
+    }
+  }
+  
+  addBook(book) {
+    this.collection.push(book);
+    localStorage.setItem('books', JSON.stringify(this.collection));
+    UI.showBooks(this.collection);
+    this.checkLength();
+  }
+  
+  removeBook(id) {
+    this.collection = this.collection.filter((book) => book.id !== id);
+    localStorage.setItem('books', JSON.stringify(this.collection));
+    UI.showBooks(this.collection);
+    this.checkLength();
+  }
+}
+  
+const library = new Library();
+UI.showBooks(library.collection);
+const title = document.getElementById('book-title');
+const author = document.getElementById('author');
+const form = document.querySelector('.form');
+const error = document.querySelector('.error');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (title.value === '' && author.value === '') {
+    error.innerHTML = 'Please fill in all the fields';
+    setTimeout(() => {
+      error.textContent = '';
+    }, 3000);
+  } else {
+    const newBook = new Book(title.value, author.value);
+    library.addBook(newBook);
+  }
+});
